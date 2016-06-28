7e98759b1f97a613d43743c9f2af322eb26e453d
Andrew-Furness <andrewjfurness1999@gmail.com>
2016-06-28 14:18:47 +0200

    Added all existing files

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+/*==============================================================================*\
+	Your Website Name
+
+	Scripts File
+
+	Happy Coding!
+
+	Author: Your Name
+	Version: 1.0.0
+\*==============================================================================*/
+$(document).ready(function() {
+    console.log("Ready to go!");
+
+		$("#portfolioButton").click(function(){
+			// $("window").scrollTo("#portfolio");
+
+		});
+		$('.js-scrolltoid').scrollToID({
+			speed: 1000, //expressed in Milliseconds
+			offset: 30   //in pixels
+		});
+
+});
